86f61365b75fb229376dbf151ad6bc42f851a291
carlosadrianmendozavalle-jpg <carlosadrianmendozavalle@gmail.com>
2026-04-27 22:30:12 -0600

    Create mis_algoritmos.py

---
+++ mis_algoritmos.py
@@ -0,0 +1,9 @@
+
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+numero3 = int(input("Ingrese el tercer número: "))
+
+if numero1 == numero2 and numero2 == numero3:
+    print("Los tres números son iguales")
+else:
+    print("Los números no son iguales")
